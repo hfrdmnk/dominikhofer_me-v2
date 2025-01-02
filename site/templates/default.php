f42af77984345d3de3c638ca9ed5fcfe3928429f
Dominik Hofer <hi@dominikhofer.me>
2025-01-02 20:09:05 +0100

    Cleaner images

--- site/templates/default.php
+++ site/templates/default.php
@@ -11,36 +11,54 @@
 <?php snippet('head') ?>
 
 <body>
-    <div class="container flex flex-col flex-gap-4">
-        <h1><?= $page->title() ?></h1>
-        <div><a href="/">Back to home</a></div>
-    </div>
+<div class="container flex flex-col flex-gap-4">
+  <h1><?= $page->title() ?></h1>
+  <div><a href="/">Back to home</a></div>
+</div>
 
-    <div class="container">
-        <div class="rt">
-            <h2>Testing</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit corporis placeat cupiditate nesciunt nobis. Est natus porro molestias nemo, molestiae corrupti excepturi quos fugiat ab sit dolorem voluptatum qui ipsam omnis accusantium in nulla tenetur nam magnam fuga. Aliquam, aut! Numquam, laudantium provident. Quia assumenda vero quasi sequi repellendus eligendi fugiat aspernatur perspiciatis obcaecati unde earum laboriosam est, repudiandae, enim quos. Veniam, quaerat delectus hic minus accusantium unde quo repellendus laborum blanditiis iusto rerum possimus architecto ea reiciendis quidem quas sequi nostrum autem reprehenderit, temporibus odio rem mollitia. Tenetur pariatur assumenda libero esse, cupiditate eveniet at ratione incidunt officiis velit?</p>
-            <ul>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-            </ul>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum est consequatur delectus quasi dolore provident obcaecati ipsum eum modi, ad esse numquam laborum nam debitis fugiat voluptatem voluptatibus dolorem voluptas enim reiciendis tempore voluptates non beatae atque! Optio animi quae ex facilis dolore, veritatis, nam ullam recusandae eaque, perferendis rem!</p>
-            <h3>Subheading</h3>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui porro perspiciatis a praesentium, minima voluptas asperiores molestias maiores ullam sapiente libero excepturi sequi pariatur recusandae fugiat blanditiis provident quibusdam officia enim eveniet. Suscipit neque pariatur voluptatum earum, tenetur unde voluptatem eligendi esse, quam assumenda animi necessitatibus accusamus inventore laborum incidunt hic eaque ullam natus corrupti sunt ea facilis beatae. Eaque, impedit? Reprehenderit sit, dicta veniam, quis quos commodi vero incidunt cum consectetur ab quibusdam blanditiis, iure aut dolores hic! Ratione repellat molestias consequuntur aliquid iusto. Soluta, adipisci consectetur debitis vitae nulla aliquam optio animi porro vel temporibus sunt! Ullam, quis?</p>
-            <h2>Another heading</h2>
-            <h3>Another subheading</h3>
-            <figure>
-                <img src="https://placehold.co/3000x4000" alt="Placeholder image">
-                <figcaption>Image caption</figcaption>
-            </figure>
-            <figure>
-                <img src="https://placehold.co/4000x3000" alt="Placeholder image">
-                <figcaption>Image caption</figcaption>
-            </figure>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum consequuntur nulla cupiditate non corporis vero atque totam nesciunt hic error repudiandae laudantium sequi placeat, temporibus quaerat. Totam voluptatibus voluptas incidunt odio facere tenetur consequuntur, asperiores et aliquid magni est sint repellat. A provident, saepe cum soluta debitis optio dolores quam.</p>
-        </div>
-    </div>
+<div class="container">
+  <div class="rt">
+    <h2>Testing</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit corporis placeat cupiditate nesciunt nobis. Est
+      natus porro molestias nemo, molestiae corrupti excepturi quos fugiat ab sit dolorem voluptatum qui ipsam omnis
+      accusantium in nulla tenetur nam magnam fuga. Aliquam, aut! Numquam, laudantium provident. Quia assumenda vero
+      quasi sequi repellendus eligendi fugiat aspernatur perspiciatis obcaecati unde earum laboriosam est, repudiandae,
+      enim quos. Veniam, quaerat delectus hic minus accusantium unde quo repellendus laborum blanditiis iusto rerum
+      possimus architecto ea reiciendis quidem quas sequi nostrum autem reprehenderit, temporibus odio rem mollitia.
+      Tenetur pariatur assumenda libero esse, cupiditate eveniet at ratione incidunt officiis velit?</p>
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </ul>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum est consequatur delectus quasi dolore
+      provident obcaecati ipsum eum modi, ad esse numquam laborum nam debitis fugiat voluptatem voluptatibus dolorem
+      voluptas enim reiciendis tempore voluptates non beatae atque! Optio animi quae ex facilis dolore, veritatis, nam
+      ullam recusandae eaque, perferendis rem!</p>
+    <h3>Subheading</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui porro perspiciatis a praesentium, minima voluptas
+      asperiores molestias maiores ullam sapiente libero excepturi sequi pariatur recusandae fugiat blanditiis provident
+      quibusdam officia enim eveniet. Suscipit neque pariatur voluptatum earum, tenetur unde voluptatem eligendi esse,
+      quam assumenda animi necessitatibus accusamus inventore laborum incidunt hic eaque ullam natus corrupti sunt ea
+      facilis beatae. Eaque, impedit? Reprehenderit sit, dicta veniam, quis quos commodi vero incidunt cum consectetur
+      ab quibusdam blanditiis, iure aut dolores hic! Ratione repellat molestias consequuntur aliquid iusto. Soluta,
+      adipisci consectetur debitis vitae nulla aliquam optio animi porro vel temporibus sunt! Ullam, quis?</p>
+    <h2>Another heading</h2>
+    <h3>Another subheading</h3>
+    <figure>
+      <div><img src="https://placehold.co/3000x4000" alt="Placeholder image"></div>
+      <figcaption>Image caption</figcaption>
+    </figure>
+    <figure>
+      <div><img src="https://placehold.co/4000x3000" alt="Placeholder image"></div>
+      <figcaption>Image caption</figcaption>
+    </figure>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum consequuntur nulla cupiditate non corporis vero
+      atque totam nesciunt hic error repudiandae laudantium sequi placeat, temporibus quaerat. Totam voluptatibus
+      voluptas incidunt odio facere tenetur consequuntur, asperiores et aliquid magni est sint repellat. A provident,
+      saepe cum soluta debitis optio dolores quam.</p>
+  </div>
+</div>
 </body>
 
 </html>
