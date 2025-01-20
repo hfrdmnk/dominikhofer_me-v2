6f83d850a4974879e799ee34840171dd4d0872e8
Dominik Hofer <hi@dominikhofer.me>
2025-01-20 21:07:47 +0100

    Worked on posts

--- site/snippets/blocks/posts-archive.php
+++ site/snippets/blocks/posts-archive.php
@@ -1,7 +1,6 @@
 <?php
 $posts = $kirby->collection('posts')->sort('date', 'desc');
 
-// create a two dimensional array of $groupedPosts[year][month] = posts
 $groupedPosts = [];
 foreach ($posts as $post) {
     $year = $post->date()->toDate('Y');
@@ -10,30 +9,29 @@ foreach ($posts as $post) {
 }
 ?>
 
-<aside class="b-posts-archive">
-    <ul class="b-posts-archive__list">
-        <?php foreach ($groupedPosts as $year => $months): ?>
-            <li>
-                <ul>
-                    <?php
-                    $monthKeys = array_keys($months);
-                    foreach ($months as $month => $posts):
-                        $isFirstMonth = array_search($month, $monthKeys) === 0;
-                    ?>
-                        <li>
-                            <h2><?php if ($isFirstMonth) echo $year ?> <?= $month ?></h2>
-                            <ul>
-                                <?php foreach ($posts as $post): ?>
-                                    <li>
-                                        <a class="no-highlight" href="<?= $post->url() ?>">
-                                            <?= $post->title() ?>
-                                        </a>
-                                    </li>
-                                <?php endforeach; ?>
-                            </ul>
-                        </li>
-                    <?php endforeach; ?>
-                </ul>
-            </li>
+<aside class="posts-archive">
+    <?php foreach ($groupedPosts as $year => $months): ?>
+        <?php
+        $monthKeys = array_keys($months);
+        foreach ($months as $month => $posts):
+            $isFirstMonth = array_search($month, $monthKeys) === 0;
+        ?>
+            <section class="posts-archive__section">
+                <h2><span><?php if ($isFirstMonth) echo $year ?></span> <span><?= $month ?></span></h2>
+                <?php foreach ($posts as $post): ?>
+                    <article class="post-item">
+                        <a class="no-highlight" href="<?= $post->url() ?>">
+                            <time><?= $post->date()->toDate("d.m.Y") ?></time>
+                            <div class="post-item__content">
+                                <h3><?= $post->title() ?></h3>
+                                <?php if ($post->featured()->isTrue()): ?>
+                                    <div class="post-item__featured"><i class="ti ti-sparkles"></i> Favorite</div>
+                                <?php endif; ?>
+                            </div>
+                        </a>
+                    </article>
+                <?php endforeach; ?>
+            </section>
         <?php endforeach; ?>
+    <?php endforeach; ?>
     </ul>
